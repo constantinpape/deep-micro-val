06353f8c8e213d70173641b2d33971f923e8b4af
Constantin Pape <constantin.pape@embl.de>
2021-10-22 23:03:23 +0200

    Update segmentation WIP

--- custom_data/shila/check_annotations.py
+++ custom_data/shila/check_annotations.py
@@ -6,23 +6,42 @@ from tqdm import tqdm
 
 
 def _to_tif(name):
-    pos = name[name.find("Pos"):name.find("Pos")+4]
+    pos = name[name.find("Pos"):].rstrip(".csv")
     return f"MMStack_{pos}.ome.tif"
 
 
-def main():
+def main(show_2d, names=None):
     annotation_folder = "./point_annotations"
-    names = os.listdir(annotation_folder)
-    data_folder = "/g/kreshuk/"
+    if names is None:
+        names = os.listdir(annotation_folder)
+        names.sort()
+
+    data_folder = "/g/kreshuk/data/marioni/shila/TimEmbryos-020420/HybCycle_29"
     for name in tqdm(names):
+
+        annotations = pd.read_csv(os.path.join(annotation_folder, name)).iloc[:, 1:]
         tif_name = _to_tif(name)
-        nuc = imageio.imread(os.path.join(data_folder, tif_name))
-        annotations = pd.read_csv(os.path.join(annotation_folder, name)).iloc[:, 2:]
+        im = imageio.volread(os.path.join(data_folder, tif_name))
+
+        if show_2d:
+            cz = annotations.iloc[0, :2]
+            annotations = annotations.iloc[:, 2:]
+            im = im[int(cz[0]), int(cz[1])]
+
         v = napari.Viewer()
-        v.add_image(nuc)
+        v.title = name
+        v.add_image(im)
         v.add_points(annotations)
         napari.run()
 
 
 if __name__ == "__main__":
-    main()
+    wrong_annotations = [
+        "TimEmbryos-020420_points_Pos10.csv",
+        "TimEmbryos-020420_points_Pos11.csv",
+        "TimEmbryos-020420_points_Pos12.csv",
+        "TimEmbryos-020420_points_Pos13.csv",
+        "TimEmbryos-020420_points_Pos15.csv",
+        "TimEmbryos-020420_points_Pos14.csv",
+    ]
+    main(show_2d=True, names=wrong_annotations)
--- custom_data/shila/segment.py
+++ custom_data/shila/segment.py
@@ -1,13 +1,87 @@
 import argparse
+import os
+
+import bioimageio.core
+import imageio
+import pandas as pd
+from tqdm import tqdm
+from bioimageio.core.prediction import predict
 
 
-def predict():
+def predict_image(model, image):
+    image = image[None, None]
+    pred = predict(model, image)[0]
+    return pred[0], pred[1:]
+
+
+def segment_seeded_watershed():
     pass
+
+
+def segment_mws():
+    pass
+
+
+def _write_res(path, seg, full_shape, cz):
+    pass
+
+
+def segment_image(model, data_path, annotation_path, seg_root, name):
+    image = imageio.volread(data_path)
+    full_shape = image.shape
+    annotations = pd.read_csv(annotation_path).iloc[:, 1:]
+
+    cz = annotations.iloc[0, :2]
+    annotations = annotations.iloc[:, 2:]
+    image = image[int(cz[0]), int(cz[1])]
+
+    fg, affs = predict_image(model, image)
+    seg_ws = segment_seeded_watershed(fg, affs, annotations)
+    seg_mws = segment_mws(fg, affs)
+
+    path_ws = os.path.join(seg_root, "watershed", name.replace(".csv", ".ome.tif"))
+    _write_res(path_ws, seg_ws, full_shape, cz)
+    path_mws = os.path.join(seg_root, "watershed", name.replace(".csv", ".ome.tif"))
+    _write_res(path_mws, seg_mws, full_shape, cz)
+
+
+def _to_tif(name):
+    pos = name[name.find("Pos"):name.find("Pos")+4]
+    return f"MMStack_{pos}.ome.tif"
+
+
+# FIXME need affinity model
+def _load_model(version, device):
+    ckpt = "./checkpoints/nuclei_v3/bioimageio-model/custom-nucleus-segmentation.zip"
+    assert os.path.exists(ckpt), ckpt
+    resource = bioimageio.core.load_resource_description(ckpt)
+    devices = None if device is None else [device]
+    model = bioimageio.core.prediction_pipeline.create_prediction_pipeline(bioimageio_model=resource, devices=devices)
+    return model
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", required=True, type=int)
+    parser.add_argument("-d", "--device", default=None, type=str)
+    args = parser.parse_args()
+
+    data_folder = "/g/kreshuk/data/marioni/shila/TimEmbryos-020420/HybCycle_29"
+    annotation_folder = "./point_annotations"
+    names = os.listdir(annotation_folder)
+    names.sort()
+
+    seg_root = "/g/kreshuk/data/marioni/shila/segmentation"
+    os.makedirs(seg_root, exist_ok=True)
+    os.makedirs(os.path.join(seg_root, "watershed"), exist_ok=True)
+    os.makedirs(os.path.join(seg_root, "mutex_watershed"), exist_ok=True)
+
+    model = _load_model(args.version, args.device)
+
+    for name in tqdm(names):
+        annotation_path = os.path.join(annotation_folder, name)
+        data_path = os.path.join(data_folder, _to_tif(name))
+        segment_image(model, data_path, annotation_path, model, seg_root, name)
 
 
 if __name__ == "__main__":
