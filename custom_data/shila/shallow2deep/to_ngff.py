ae25c750ea7262f3e7de748577eaf705b27234af
Constantin Pape <constantin.pape@embl.de>
2022-02-28 21:33:40 +0100

    Update ngff converter

--- custom_data/shila/shallow2deep/to_ngff.py
+++ custom_data/shila/shallow2deep/to_ngff.py
@@ -4,28 +4,59 @@ from glob import glob
 
 import imageio
 import ome_zarr
+import ome_zarr.scale
+import ome_zarr.writer
 import zarr
 from tqdm import tqdm
 
 
 def convert_image_data(in_path, out_path):
-    vol = imageio.volread(in_path)
-    axes = tuple("zcyx")
 
+    # load the input data from ome.tif
+    vol = imageio.volread(in_path)
+    # the data is stored as 'zcyx'. This is currently not allowed by ome.zarr, so we reorder to 'czyx'
+    vol = vol.transpose((1, 0, 2, 3))
+
+    # create scale pyramid
+    # TODO how do we set options for the scaling?
+    # (in this case the defaults are fine,
+    # but it should be possible to over-ride this in general)
     scaler = ome_zarr.scale.Scaler()
     mip = scaler.local_mean(vol)
-    # TODO check the MIP
-    loc = ome_zarr.io.parse_url(out_path, mode="w")
-    group = zarr.group(loc.store)
 
-    # TODO
+    # specify the axis and scale metadata
+    axes_names = tuple("czyx")
+    # TODO get this programatically / from data passed to the scaler
+    is_scaled = {"c": False, "z": False, "y": True, "x": True}
+
+    # TODO get resolution info and units from shila
+    resolution = {"c": 1.0, "z": 1.0, "y": 1.0, "x": 1.0}
+    units = [None, "pixel", "pixel", "pixel"]
+    types = ["channel", "space", "space", "space"]
+
     trafos = [
-        [{"type": "scale", "scale": [1.]}]
+        [{
+            "scale": [resolution[ax] * 2**scale_level if is_scaled[ax] else resolution[ax] for ax in axes_names],
+            "type": "scale"
+        }]
+        for scale_level in range(len(mip))
     ]
 
+    axes = []
+    for ax, type_, unit in zip(axes_names, types, units):
+        axis = {"name": ax, "type": type_}
+        if unit is not None:
+            axis["unit"] = unit
+        axes.append(axis)
+
+    # provide additional storage options for zarr
     chunks = (1, 1, 512, 512)
-    assert len(chunks) == len(axes)
-    storage_opts = {"compression": "gzip", "chunks": chunks}
+    assert len(chunks) == len(axes_names)
+    storage_opts = {"chunks": chunks}
+
+    # write the data to ome.zarr
+    loc = ome_zarr.io.parse_url(out_path, mode="w")
+    group = zarr.group(loc.store)
     ome_zarr.writer.write_multiscale(
         mip, group, axes=axes,
         coordinate_transformations=trafos, storage_options=storage_opts,
@@ -67,7 +98,6 @@ def main():
         assert os.path.exists(nucleus_segmentation)
         # TODO
         # convert_label_data()
-        return
 
 
 # starting point:
